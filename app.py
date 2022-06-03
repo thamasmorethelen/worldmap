287636027e54cac34dff235fa06b25a68253733b
Thomas Thelen <thamasthelen@gmail.com>
2022-06-02 22:51:25 -0700

    app.py updates

--- app.py
+++ app.py
@@ -1,19 +1,40 @@
 import folium
 import pandas
 
-map = folium.Map(location=[38.58, -99.09], zoom_start=6, titles="Stamen Terrain")
 
-fg = folium.FeatureGroup(name="My Map")
-
-data = pandas.read_csv('world_map_data/Volcanoes.txt')
-
-lat = list(data["LAT"])
-lon = list(data['LON'])
-name = list(data['NAME'])
-
-for lt, ln, name in zip(lat, lon, name):
-    fg.add_child(folium.Marker(location=[lt, ln], popup=name, icon=folium.Icon(color='green')))
+def to_feet(listy):
+    ls = []
+    item = ''
+    for i in listy:
+        item = i * 3.28084
+        item = round(item, 2)
+        ls.append(item)
+    return ls
 
 
-map.add_child(fg)
-map.save('Map1.html')
+def map():
+    map = folium.Map(location=[38.58, -99.09], zoom_start=6, titles="Stamen Terrain")
+    return map
+
+
+map = map()
+
+
+def make_map():
+    fg = folium.FeatureGroup(name="My Map")
+
+    data = pandas.read_csv('world_map_data/Volcanoes.txt')
+    name = list(data['NAME'])
+    elev = to_feet(list(data['ELEV']))
+    tipe = list(data['TYPE'])
+    lat = list(data["LAT"])
+    lon = list(data['LON'])
+    
+    for lt, ln, name, elev, tipe in zip(lat, lon, name, elev, tipe):
+        fg.add_child(folium.Marker(location=[lt, ln], popup=f'Name: {name} \nType: {tipe} \nElevation: {elev} feet', icon=folium.Icon(color='green')))
+    map.add_child(fg)
+    map.save('Map1.html')
+    return map
+
+
+make_map()
