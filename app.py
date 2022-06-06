c3b28e1c612676020c35240ac7772197580d610e
Thomas Thelen <thamasthelen@gmail.com>
2022-06-06 14:22:21 -0700

    Updates to app.py
    added html

--- app.py
+++ app.py
@@ -22,18 +22,27 @@ map = map()
 
 def make_map():
     fg = folium.FeatureGroup(name="My Map")
-
-    data = pandas.read_csv('world_map_data/Volcanoes.txt')
+    data = pandas.read_csv("world_map_data/Volcanoes.txt")
+    html = """
+    Volcano name:<br>
+    <a href="https://www.google.com/search?q=%%22%s%%22" target="_blank">%s</a><br>
+    Height: %s feet<br>
+    Type: %s
+    """
     name = list(data['NAME'])
     elev = to_feet(list(data['ELEV']))
     tipe = list(data['TYPE'])
     lat = list(data["LAT"])
     lon = list(data['LON'])
 
+
+
+
     for lt, ln, name, elev, tipe in zip(lat, lon, name, elev, tipe):
-        fg.add_child(folium.Marker(location=[lt, ln], popup=f'Name: {name} \nType: {tipe} \nElevation: {elev} feet', icon=folium.Icon(color='green')))
+        iframe = folium.IFrame(html=html % (name, name, elev, tipe), width=200, height=100)
+        fg.add_child(folium.Marker(location=[lt, ln], popup=folium.Popup(iframe), icon=folium.Icon(color='green')))
     map.add_child(fg)
-    map.save('Map1.html')
+    map.save("Map_html_popup_advanced.html")
     return map
 
 
